86ebd8b08083e15dde1802c262d2d6ad416f8ef9
Amit Kannaujiya <kannaujiya.amitdm@investors-clinic.com>
2020-04-05 23:32:36 +0530

    commentation

--- js/kaios.js
+++ js/kaios.js
@@ -45,8 +45,8 @@ var MODULE = (function () {
     });
 
     /**
-     * initilize view componsents,
-     *  
+     * get json data from data.json and parse same 
+     * and show them in UI div with the help of jquery.
      */
     function initView() {
         appendDate();
@@ -56,49 +56,34 @@ var MODULE = (function () {
         let columnHeadings = payload.columnHeadings;
         let rows = payload.rows[0];
         detailDiv = $('#header-detail');
-        for(let [index, row] of Object.entries(rows)) {
-            if(index > app.totalNoOfStates) {
+        for (let [index, row] of Object.entries(rows)) {
+            if (index > app.totalNoOfStates) {
                 $(detailDiv).append(
-                    '<div class="card m-t10 crs-pointer"><div class="header-title-div" onclick="showDetail(' +"'detail-"+30+  "')"+ '">'
-                    +'<div class="state-name"> <strong class="state-name-span">'+ row[0]+' :</strong></div>'
-                    +'</div>'
-                    + '<div class="detail-'+30+' header-detail">'
-                    +    '<div><strong>Total Confirmed cases : '+ row[1]+'</strong></div>'
-                    +    '<div><strong class="cured-count-span">Total Cured/Discharged/Migrated : '+ row[3] + '</strong></div>'
-                    +    '<div><strong class="death-count-span">Total Deaths : ' + row[6] + '</strong></div>'
-                    +  '</div>'
-                    +  '<div>'
-                ); 
-                $('#show-detail').toggleClass('hide');       
+                    '<div class="card m-t10 crs-pointer"><div class="header-title-div" onclick="showDetail(' + "'detail-" + 30 + "')" + '">'
+                    + '<div class="state-name"> <strong class="state-name-span">' + row[0] + ' :</strong></div>'
+                    + '</div>'
+                    + '<div class="detail-' + 30 + ' header-detail">'
+                    + '<div><strong>Total Confirmed cases : ' + row[1] + '</strong></div>'
+                    + '<div><strong class="cured-count-span">Total Cured/Discharged/Migrated : ' + row[3] + '</strong></div>'
+                    + '<div><strong class="death-count-span">Total Deaths : ' + row[6] + '</strong></div>'
+                    + '</div>'
+                    + '<div>'
+                );
+                $('#show-detail').toggleClass('hide');
                 break;
             }
-        // for (var index = 0; index < rows.length && index < app.totalNoOfStates; index++) {
 
             $(detailDiv).append(
-                '<div class="card m-t10 crs-pointer"><div class="header-title-div" onclick="showDetail(' +"'detail-"+row[0]+  "')"+ '">'
-                +'<div class="state-name"> <strong>Name of State / UT : </strong><span class="state-name-span">'+ row[1]+'</span></div>'
-                +'</div>'
-                + '<div class="detail-'+row[0]+' header-detail">'
-                +    '<div><strong class="state-name-span">Total Confirmed cases : '+ row[2]+'</strong></div>'
-                +    '<div><strong class="cured-count-span">Cured/Discharged/Migrated : '+ row[3] + '</strong></div>'
-                +    '<div><strong class="death-count-span">Death : ' + row[4] + ' </strong></div>'
-                +  '</div>'
-                +  '<div>'
+                '<div class="card m-t10 crs-pointer"><div class="header-title-div" onclick="showDetail(' + "'detail-" + row[0] + "')" + '">'
+                + '<div class="state-name"> <strong>Name of State / UT : </strong><span class="state-name-span">' + row[1] + '</span></div>'
+                + '</div>'
+                + '<div class="detail-' + row[0] + ' header-detail">'
+                + '<div><strong class="state-name-span">Total Confirmed cases : ' + row[2] + '</strong></div>'
+                + '<div><strong class="cured-count-span">Cured/Discharged/Migrated : ' + row[3] + '</strong></div>'
+                + '<div><strong class="death-count-span">Death : ' + row[4] + ' </strong></div>'
+                + '</div>'
+                + '<div>'
             );
-
-            // <div class="card">
-            //     <div class="header-title-div" onclick="showDetail('detail-1')">
-            //         <div class="state-name"><strong>Name of State / UT : </strong> Andhra Pradesh</div>
-            //     </div>
-            //     <div class="detail-1 header-detail">
-            //         <div><strong>Total Confirmed cases : </strong> 161</div>
-            //         <div><strong>Cured/Discharged/Migrated : </strong> 1</div>
-            //         <div><strong>Death : </strong> 1</div>
-            //         <div>
-
-            //         </div>
-
-            //     </div>
         }
 
     }
@@ -113,21 +98,21 @@ var MODULE = (function () {
      *  failed due to setup of server on the localhost:port
      */
     function fetchCrawledData() {
-                    $.getJSON("data1.json", function () {
-                        console.log("success");
-                    }).done(function (data) {
-                        console.log("second success");
-                    })
-                        .fail(function () {
-                            console.log("error");
-                        })
-                        .always(function () {
-                            console.log("complete");
-                        });
+        $.getJSON("data1.json", function () {
+            console.log("success");
+        }).done(function (data) {
+            console.log("second success");
+        })
+            .fail(function () {
+                console.log("error");
+            })
+            .always(function () {
+                console.log("complete");
+            });
 
 
-        $.getJSON( '../data1.json', function( data ) {
-                    console.log(data);
+        $.getJSON('../data1.json', function (data) {
+            console.log(data);
         });
 
         //usage:
@@ -148,7 +133,7 @@ var MODULE = (function () {
         rawFile.open("GET", file, true);
         rawFile.onreadystatechange = function () {
             if (rawFile.readyState === 4 && rawFile.status == "200") {
-                    callback(rawFile.responseText);
+                callback(rawFile.responseText);
             }
         }
         rawFile.send(null);
