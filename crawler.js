2be3da08ab1ba6a96c587c38d4af2d9a27909f9d
Amit Kannaujiya <kannaujiya.amitdm@investors-clinic.com>
2020-04-06 20:55:55 +0530

    synced latest data

--- crawler.js
+++ crawler.js
@@ -2,7 +2,6 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 var fs = require('fs');
-var util = require('util');
 
 var pageToVisit = "https://www.mohfw.gov.in/";
 
@@ -10,6 +9,9 @@ var path = "data.json";
 
 console.log("Visiting page " + pageToVisit);
 
+/**
+ * Request Page Link and parse
+ */
 request(pageToVisit, function (error, response, body) {
     if (error) {
         console.log("Error: " + error);
@@ -29,28 +31,37 @@ request(pageToVisit, function (error, response, body) {
             rows: jsonReponse,
         }
 
-
+        /**
+         * Parse table header from page, and store column heading
+         */
         $('#state-data .table-striped').find('thead').each(function(i, table) {
             $(this).find('tr').children().each(function(index, tr){
                 columnHeadings.push($(this).text().trim());
             });
         });
 
+        /**
+         * Parse table rows from page, and store rows 
+         */
         $('#state-data .table-striped').find('tbody').each(function(i, table) {
             const rowJson = {};
             $(this).find('tr').each(function(index, tr){
                 let row = $(this).text().trim();
                 rowJson[ index ] = row.split('\n\t');
             });
-            console.log(rowJson);
-            console.log("newt line \n");
+
             if (JSON.stringify(rowJson) !== '{}') {
                 jsonReponse.push(rowJson);
             }
         });
 
         console.log(msg);
-
+        
+        /**
+         * Store parsed data in data.json file
+         * @param  data 
+         * @param /data.json 
+         */
         const storeData = (data, path) => {
             try {
               fs.writeFileSync(path, "var coronaStatistics=" + JSON.stringify(data))
